fa6890885c0c13be249c26acbeb3fff158cd01b8
Jack Scherlag <jack.scherlag@gmail.com>
2021-03-10 15:43:13 -0500

    Added sub and subu

--- src/mipsSimulator.cpp
+++ src/mipsSimulator.cpp
@@ -264,6 +264,36 @@ void rHelper(uint32_t instruction, uint32_t* pc, uint32_t* regs) {
 
 	break;
       }
+      // sub signed
+        case 0x22:
+	  {
+	  uint32_t rd = createMask(11, 15, instruction);
+	  uint32_t rt = createMask(16, 20, instruction);
+	  uint32_t rs = createMask(21, 25, instruction);
+
+	  int op1 = regs[rs];
+	  int op2 = regs[rt];
+	  
+	  regs[rd] = op1 - op2;
+	 
+	  *pc = *pc + 4;
+	  break;
+	  }
+       // sub unsigned
+       case 0x23:
+        {
+	uint32_t rd = createMask(11, 15, instruction);
+	uint32_t rt = createMask(16, 20, instruction);
+	uint32_t rs = createMask(21, 25, instruction);
+
+	uint32_t op1 = regs[rs];
+	uint32_t op2 = regs[rt];
+	  
+        regs[rd] = op1 - op2;
+	
+	*pc = *pc + 4;
+	break;
+      }
       
       
 
@@ -289,7 +319,8 @@ int main(int argc, char** argv) {
 
     // create bank of registers
     uint32_t regs[NUMREGS] = {0};
-    regs[9] = 1;
+    regs[8] = -1;
+    regs[9] = -1;
 
     // create program counter
     uint32_t pc = 0;
